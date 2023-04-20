64adeb26e8dcdc2e3584dc877be089f3a0f4392b
Sion <sh1670@bristol.ac.uk>
2023-04-20 20:39:18 +0100

    See 1.py for start being made on transferring what you did

--- 1.py
+++ 1.py
@@ -2,11 +2,17 @@ import json
 import numpy as np
 import pathlib
 from scipy import interpolate
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+
 import matplotlib.pyplot as plt
 
 # globals
-vid_details_root = 'F:\\WORK\\DATASETS\\jk_data_only_22\\video_details\\'
-squat_root = 'F:\\WORK\\DATASETS\\jk_data_only_22\\openposedata\\'
+#vid_details_root = 'F:\\WORK\\DATASETS\\jk_data_only_22\\video_details\\'
+#squat_root = 'F:\\WORK\\DATASETS\\jk_data_only_22\\openposedata\\'
+
+vid_details_root = 'C:\\TEACHING\\DATASETS\\jk_data_only_22\\video_details\\'
+squat_root = 'C:\\TEACHING\\DATASETS\\jk_data_only_22\\openposedata\\'
 
 class VidObj:
     def __init__(self, class_labels, in_and_outs):
@@ -47,45 +53,6 @@ def read_squat_edit(directory, start_frame, end_frame):
 
     return pose_data
 
-def read_squat(directory, start_frame, end_frame):
-    pose_data = np.zeros((18, end_frame-start_frame))
-    all_files = sorted(pathlib.Path(directory).glob("*.json"))
-    for i, file_path in enumerate(all_files[start_frame:end_frame]):
-        with open(file_path, "r") as read_file:
-            data = json.load(read_file)
-            if i == 0:
-                squat_index = 0
-            elif data['people']:
-                keypoints = np.array(data['people'][0]['pose_keypoints_2d']).reshape(-1, 3).T
-                hips_y = (keypoints[1, 11] + keypoints[1, 12]) / 2
-                if hips_y < prev_hips_y:
-                    squat_index += 1
-            prev_hips_y = hips_y
-            
-            if squat_index >= len(vo.class_labels):
-                break
-                
-            expected_label = vo.class_labels[squat_index]
-            actual_label = "correct"
-            if i < vo.in_and_outs[squat_index*2] - start_frame or i >= vo.in_and_outs[squat_index*2+1] - start_frame:
-                actual_label = "incorrect_not_low"
-            else:
-                keypoints = np.array(data['people'][0]['pose_keypoints_2d']).reshape(-1, 3).T
-                if keypoints[1, 0] - keypoints[1, 8] < -100:
-                    actual_label = "incorrect_lean_fwd"
-                elif keypoints[1, 15] - keypoints[1, 16] < 0:
-                    actual_label = "incorrect_chin_tuck"
-                elif keypoints[0, 0] - keypoints[0, 4] < -100:
-                    actual_label = "incorrect_feet_close"
-                    
-            if actual_label == expected_label:
-                print(f"Frame {i} is Correct Squat")
-            else:
-                print(f"Frame {i} is {actual_label}")
-                
-            pose_data[:, i] = np.array(data['people'][0]['pose_keypoints_2d']).reshape(-1, 3).T[:, :-1].flatten()
-            
-    return pose_data
 
 all_labels = []
 all_squats = []
@@ -135,12 +102,18 @@ for i in range(0, len(all_squats_same_cols)):
     X[:, i] = squat_curr.flatten()
     print(2)
 
-print(1)
-
 # Do PCA on X such that X still has num_squats (296?) columns but much less rows
 # Train SVM and get validation data with X and a new vector y that you make from the all_labels
+#Use PCA to reduce the dimensionality of the squat data to 2 dimensions:
+pca = PCA(n_components=25)
+X_pca = pca.fit_transform(X.T) # Note X was transposed so that we now have 296 squats with each represented by 25 features
+
+#Train an SVM classifier on the PCA-transformed data:
+svm = SVC()
+svm.fit(X_pca, all_labels)
 
 
+print(1)
 
 
 
